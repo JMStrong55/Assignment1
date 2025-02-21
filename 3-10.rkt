1ee6719f643e50299ac0a6afa1721425f0bde209
JMStrong55 <147786299+JMStrong55@users.noreply.github.com>
2025-02-21 06:40:12 -0600

    Update 3-10.rkt

--- 3-10.rkt
+++ 3-10.rkt
@@ -5,3 +5,11 @@
         (* 2 (pascal (- n 1)))))
 
 (define (pascal-display n) display (pascal n))
+
+;Question 4
+(define (deleteitem lst)
+      (if (< (length lst) 3)
+          lst
+          (append (list (car lst))
+                  (list (cadr lst))
+                  (cdddr lst))))
